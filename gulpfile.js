4156e07a5c490592e22a50029cdfa8ebdb010916
nlicitra <nickerous@gmail.com>
2016-03-06 14:30:17 -0500

    Fixed project structure and automated server startup

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,49 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var mustache = require('gulp-mustache');
+var fs = require('fs');
 var babel = require('gulp-babel');
+var python = require('python-shell');
+var del = require('del');
 var _ = require('lodash/core');
 
 // Compile SCSS into CSS
 gulp.task('sass', function() {
-	return gulp.src('./styles/**/*.scss')
+	return gulp.src('./src/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('main.css'))
-		.pipe(gulp.dest('./static/styles/'));
+		.pipe(gulp.dest('./dist/styles/'));
 });
 
 gulp.task('sass:watch', function() {
-	return gulp.watch('./styles/**/*.scss', ['sass']);
+	return gulp.watch('./src/**/*.scss', ['sass']);
 });
 
 gulp.task('es6', function() {
-	return gulp.src('./scripts/**/*.es6')
+	return gulp.src('./src/**/*.es6')
 		.pipe(babel({
 			presets: ['es2015']
 		}))
 		.pipe(concat('main.js'))
-		.pipe(gulp.dest('./static/scripts/'));
+		.pipe(gulp.dest('./dist/scripts/'));
 });
 
 gulp.task('es6:watch', function() {
-	return gulp.watch('./scripts/**/*.es6', ['es6']);
+	return gulp.watch('./src/**/*.es6', ['es6']);
+});
+
+gulp.task('html', function() {
+	var templateData = {};
+	templateData.style = !!fs.existsSync('./dist/scripts/main.js');
+	templateData.script = !!fs.existsSync('./dist/styles/main.css');
+	if (!templateData.script && !templateData.style) {
+		templateData.message = "It worked! Now get to coding!";
+	}
+
+	return gulp.src('./src/index.html')
+		.pipe(mustache(templateData))
+		.pipe(gulp.dest('dist'));
 });
 
 var libraries = ['d3', 'lodash']
@@ -36,9 +53,19 @@ gulp.task('lib', function() {
 	});
 	return gulp.src(libPaths)
 		.pipe(concat('libraries.js'))
-		.pipe(gulp.dest('./static/scripts/'))
+		.pipe(gulp.dest('./dist/scripts/'))
+});
+
+gulp.task('clean', function() {
+	del(['./dist/**/*']);
+});
+
+gulp.task('server', function() {
+	python.run('server.py', function (error) {
+		if (error) throw error;
+	});
 });
 
 gulp.task('watch', ['sass:watch', 'es6:watch']);
-gulp.task('start', ['lib', 'watch']);
+gulp.task('start', ['clean', 'lib', 'es6', 'sass', 'html', 'watch', 'server']);
 gulp.task('default', ['start']);
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='dist')
+app.config['STATIC_FOLDER'] = 'dist'
 
 @app.route('/')
 def index():
